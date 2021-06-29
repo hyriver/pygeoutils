4d5843c2ee1c388aa3cae2551601603384de8c12
cheginit <cheginit@gmail.com>
2021-06-29 18:03:18 -0500

    ENH: Add a new exception called MissingAttribute [skip ci]

--- pygeoutils/__init__.py
+++ pygeoutils/__init__.py
@@ -1,9 +1,16 @@
 """Top-level package for PyGeoUtils."""
 from pkg_resources import DistributionNotFound, get_distribution
 
-from .exceptions import InvalidInputType, InvalidInputValue
+from .exceptions import InvalidInputType, InvalidInputValue, MissingAttribute
 from .print_versions import show_versions
-from .pygeoutils import MatchCRS, arcgis2geojson, gtiff2xarray, json2geodf, geo2polygon
+from .pygeoutils import (
+    MatchCRS,
+    arcgis2geojson,
+    geo2polygon,
+    gtiff2xarray,
+    json2geodf,
+    xarray_geomask,
+)
 
 try:
     __version__ = get_distribution(__name__).version
