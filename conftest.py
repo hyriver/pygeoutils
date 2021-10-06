64bba5c0f4b0dfca57ff87ee6b60b6e7cd355e60
cheginit <cheginit@gmail.com>
2021-10-06 16:21:22 -0500

    ENH: Add a new fixure for a sample WMS response

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
 """Configuration for pytest."""
 
 import pytest
+from pygeoogc import WMS, ServiceURL
+from shapely.geometry import Polygon
+
+DEF_CRS = "epsg:4326"
+GEO_NAT = Polygon(
+    [[-69.77, 45.07], [-69.31, 45.07], [-69.31, 45.45], [-69.77, 45.45], [-69.77, 45.07]]
+)
 
 
 @pytest.fixture(autouse=True)
@@ -9,3 +16,19 @@ def add_standard_imports(doctest_namespace):
     import pygeoutils as geoutils
 
     doctest_namespace["geoutils"] = geoutils
+
+
+@pytest.fixture()
+def wms_resp():
+    """Return a WMS response."""
+    wms = WMS(
+        ServiceURL().wms.mrlc,
+        layers="NLCD_2011_Tree_Canopy_L48",
+        outformat="image/geotiff",
+        crs=DEF_CRS,
+    )
+    return wms.getmap_bybox(
+        GEO_NAT.bounds,
+        1e3,
+        box_crs=DEF_CRS,
+    )
