a7412e177ac6a691ef078b64240e3bf7ca5ff728
cheginit <cheginit@gmail.com>
2021-12-07 21:44:41 -0600

    ENH: Run the local tests with py3.10 [skip ci]

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,10 @@
+import shutil
+from pathlib import Path
+
 import nox
 
 
-@nox.session(python="3.9")
+@nox.session(python="3.10")
 def tests(session):
     session.install(".[test]")
     hr_deps = ["pygeoogc"]
@@ -10,3 +13,12 @@ def tests(session):
     session.run("pytest")
     session.run("coverage", "report")
     session.run("coverage", "html")
+
+    dirs = [".pytest_cache", "build", "dist", ".eggs"]
+    for d in dirs:
+        shutil.rmtree(d, ignore_errors=True)
+
+    patters = ["*.egg-info", "*.egg", "*.pyc", "*~", "**/__pycache__"]
+    for p in patters:
+        for f in Path.cwd().rglob(p):
+            shutil.rmtree(f, ignore_errors=True)
